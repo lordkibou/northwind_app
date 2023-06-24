eb9060e8997a414d82a16e44247e852f5f9fe57b
JuanDiaz <juanben10xlr99@gmail.com>
2023-06-24 18:45:18 +0100

    funcion-cargarProductosCategoria-cargarProductosFunc-SELECT-Fixed

--- logica/funciones/cargarProductosCategoria.js
+++ logica/funciones/cargarProductosCategoria.js
@@ -1,19 +1,20 @@
 // ----------------------------------------------------------------------------
-//
-//            cargarProductos() <--
-//         <--
+//  JSON{CategoryID:Natural} -->
+//                      cargarProductosCategoria() <--
+//                  <--
 // Lista<JSON{ProductID:Natural, ProductName:Texto, CategoryName:Texto, Unit:Texto,Price:Real}>
 // ----------------------------------------------------------------------------
 
-module.exports = async function cargarProductos() {
+module.exports = async function cargarProductosCategoria( datos ) {
 
     var textoSQL = `SELECT Products.ProductID, Products.ProductName, Categories.CategoryName, Products.Unit, Products.Price
     FROM Products
-    JOIN Categories ON Products.CategoryID = Categories.CategoryID;    
+    JOIN Categories ON Products.CategoryID = Categories.CategoryID
+    WHERE Categories.CategoryID = $CategoryID; 
     `;
-
+    var valoresParaSQL = { $CategoryID: datos.CategoryID }
     return new Promise( (resolver, rechazar) => {
-        cargarProductos.conexion.all( textoSQL,
+        cargarProductosCategoria.conexion.all( textoSQL, valoresParaSQL,
             ( err, res ) => {
                 ( err ? rechazar(err) : resolver(res) )
             })
